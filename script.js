1c7a45f630a0364f63b41569cd30d5b0b0119afb
salamander <agrawal.sheetal110@gmail.com>
2025-04-27 02:30:29 -0400

    Update

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 document.getElementById('theme-toggle').addEventListener('click', function() {
     const body = document.body;
     const themeText = document.getElementById('theme-text');
-    const themeToggle = document.getElementById('theme-toggle');
     
     if (body.classList.contains('white-theme')) {
         body.classList.remove('white-theme');
         body.classList.add('black-theme');
         themeText.textContent = "Is this White?";
-        themeToggle.textContent = "Sure?";
     } else {
         body.classList.remove('black-theme');
         body.classList.add('white-theme');
         themeText.textContent = "Is this Black?";
-        themeToggle.textContent = "Sure?";
     }
 });
